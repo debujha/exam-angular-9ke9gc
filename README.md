2edac00a1f44271386f570ec1a1a38cbe6f829e7
debujha <debtujha@gmail.com>
2022-02-24 14:37:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# exam-angular-9ke9gc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/exam-angular-9ke9gc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+
+import { InvoiceComponent } from './invoice/invoice.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InvoiceComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
